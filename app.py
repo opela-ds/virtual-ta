922932379f1db390a30e92c78f76051148eb983a
opela-ds <24f1001750@ds.study.iitm.ac.in>
2025-06-14 12:27:24 +0530

    Deleted setup files

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def virtual_ta():
         }), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Critical for Render
+    port = int(os.environ.get("PORT", 10000))  # Critical for Render
     app.run(host='0.0.0.0', port=port)
